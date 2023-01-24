64910c6c95b76f843e1e470dc8ddeeda3948112f
CapaCC1 <nicolascapatina29@gmail.com>
2023-01-24 22:19:02 +0100

    Actualizacion Bascula

--- src/Bascula/Persona.java
+++ src/Bascula/Persona.java
@@ -1,5 +1,87 @@
 package Bascula;
 
 public class Persona {
+	private String nombre;
+	private String dni;
+	private int edad;
+	private String sexo;
+	private double peso;
+	private double altura;
+	
+	Persona(String nombre, String dni, int edad, String sexo, double peso, double altura){
+		this.nombre = nombre;
+		this.dni = dni;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.peso = peso;
+		this.altura = altura;
+		
+	}
+	Persona(){
+		this.sexo = "H";
+		this.nombre = "";
+		this.dni = "";
+		this.edad = 0;
+		this.peso = 0;
+		this.altura = 0;
+	}
+	
+	Persona(String nombre, int edad, String sexo){
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		}
+	
+	// Métodos de acceso
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+	
+	public double determina_IMC() {
+		return peso / (altura * altura);
+	}
+	
+	public boolean esMayordeEdad() {
+		return edad > 18;
+	}
+	
+	public String toString() {
+        return "Nombre: " + nombre + ", Edad: " + edad + ", DNI: " + dni + ", Sexo: " + sexo + ", Peso: " + peso + "kg, Altura: " + altura + "m, IMC: ";
+    }
 }
---
+++ src/Bascula/PruebaPersona.java
@@ -0,0 +1,100 @@
+package Bascula;
+import java.util.Scanner;
+public class PruebaPersona {
+
+	public static String comprobarIMC(double imc, String nombre1) {
+		String resultado = "";
+		if(imc < 18.5) {
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado += (" -> Peso Insuficiente ");
+			
+		}else if(imc > 18.5 && imc < 24.9){
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado+=(" -> Normopeso");
+			
+		}else if(imc > 25 && imc < 26.9) {
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado += (" -> Sobrepeso Grado 1");
+			
+		}else if(imc > 27 && imc < 29.9) {
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado += (" -> Sobrepeso Grado 2 (Preobesidad)");
+			
+		}else if(imc > 30 && imc < 34.9) {
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado += (" -> Obesidad Tipo 1");
+			
+		}else if(imc > 35 && imc < 39.9) {
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado += (" -> Obesidad Tipo 2");
+			
+		}else if(imc > 40 && imc < 49.9) {
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado += (" -> Obesidad Tipo 3(Morbida)");
+			
+		}else if(imc > 50) {
+			resultado += ("El IMC de " + nombre1 + " es " + imc);
+			resultado += (" -> Obesidad Tipo 3(Extrema)");
+			
+		}
+		return resultado;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner in1 = new Scanner (System.in);
+		boolean salir = false;
+		System.out.println("--PROGRAMA BASCULA--");
+		
+		while(!salir) {
+			
+			System.out.println();
+			System.out.println("1-Persona 1");
+			System.out.println("2-Persona 2");
+			System.out.println("3-Persona 3");
+			System.out.println();
+			System.out.println("Elija la Opcion Deseada: ");
+			int opcion = in1.nextInt();
+			switch(opcion) {
+			case 1:
+				System.out.println("PERSONA 1");
+				System.out.println("\tObtencion de Atributos por Teclado");
+				System.out.println();
+				System.out.println("Introduzca el NOMBRE de la primera PERSONA");
+				String nombre1 = in1.next();
+				System.out.println("Introduzca la EDAD de la primera PERSONA");
+				int edad1 = in1.nextInt();
+				System.out.println("Introduca el SEXO de la primera PERSONA");
+				String sexo1 = in1.next();
+				System.out.println("Introduzca el PESO de la primera PERSONA");
+				double peso1 = in1.nextDouble();
+				System.out.println("Introduzca la ALTURA de la primera PERSONA");
+				double altura1 = in1.nextDouble();
+				Persona persona1 = new Persona(nombre1, "123456789X", edad1, sexo1, peso1, altura1);	
+				double imc = persona1.determina_IMC();
+				System.out.println(comprobarIMC(imc, nombre1));
+				
+				if(persona1.esMayordeEdad()) {
+					System.out.println( nombre1 + " es mayor de edad");
+				}
+				
+				break;
+			case 2:
+				System.out.println("PERSONA 2");
+				System.out.println("\tAl Objeto se le pasan por parámetro el nombre, la edad y el sexo.\n "
+						+ "El peso y la altura le serán proporcionados a través de los métodos set");
+				Persona persona2 = new Persona("Nicolas", "123456789X", 25, "H", 85, 1.75);
+				break;
+			case 3:
+				System.out.println("PERSONA 3");
+				System.out.println("Se inicializa por parametros directamente ");
+				break;
+			}
+		}
+		
+		
+		
+		
+	}
+
+}
